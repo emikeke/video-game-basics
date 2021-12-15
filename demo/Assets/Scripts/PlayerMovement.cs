f65671bac73a8ee9e543bbe74f374dfbed16f686
Emily K <emily.ke@hotmail.com>
2021-12-15 11:20:57 -0500

    added script to track player movement;

---
+++ demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // References the Rigidbody component "rb";
+    public Rigidbody rb;
+
+    public float forwardForce = 200f;
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        // Adds force to player cube
+        rb.AddForce(Input.GetAxis("Horizontal") * forwardForce * Time.deltaTime, 0, 0);
+        rb.AddForce(0, 0, Input.GetAxis("Vertical") * forwardForce * Time.deltaTime);
+    }
+}
